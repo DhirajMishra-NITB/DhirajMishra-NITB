edca15b8391be851e8de3a59f3a7cc797a65d0aa
DhirajMishra-NITB <91793064+DhirajMishra-NITB@users.noreply.github.com>
2021-10-02 21:36:59 +0530

    Update Strings.py
    Added Solution of Excercise 1.3

--- Strings.py
+++ Strings.py
@@ -33,3 +33,20 @@ def both_ends(s):
     
 
 both_ends('s')
+
+# C. fix_start
+# Given a string s, return a string
+# where all occurences of its first char have
+# been changed to '*', except do not change
+# the first char itself.
+# e.g. 'babble' yields 'ba**le'
+# Assume that the string is length 1 or more.
+# Hint: s.replace(stra, strb) returns a version of string s
+# where all instances of stra have been replaced by strb.
+def fix_start(s):
+    ch=s[0]
+    s=s.replace(s[0],"*")
+    print(ch+s[1:])
+    
+fix_start("babble")
+    
