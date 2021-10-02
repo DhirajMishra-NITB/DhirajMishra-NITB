2073cabbfc977b11cc01021392affcff1ba638ea
DhirajMishra-NITB <91793064+DhirajMishra-NITB@users.noreply.github.com>
2021-10-02 22:49:27 +0530

    Update Strings.py
    Added Test Cases along with main Function

--- Strings.py
+++ Strings.py
@@ -11,16 +11,14 @@ def donuts(count):
   if count >= 10:
         return 'Number of donuts: many'
   else:
-        return 'Number of donuts:{0:d}'.format(count)
-
-donuts(5)
-##donuts(15)
-
+        return 'Number of donuts:{0: d}'.format(count)
+    
 # B. both_ends
 # Given a string s, return a string made of the first 2
 # and the last 2 chars of the original string,
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
+    
 def both_ends(s):
   if len(s)<2:
     return ''
@@ -28,12 +26,10 @@ def both_ends(s):
     r = s.split(' ')
     t = r[0]
     w = t[0:2]
-    x=t[::-1]
-    print(w+x[0:2])
+    x=t[-2:]
     
-
-both_ends('s')
-
+    return(w+x)
+  
 # C. fix_start
 # Given a string s, return a string
 # where all occurences of its first char have
@@ -43,10 +39,79 @@ both_ends('s')
 # Assume that the string is length 1 or more.
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
+    
 def fix_start(s):
     ch=s[0]
     s=s.replace(s[0],"*")
-    print(ch+s[1:])
+    return(ch+s[1:])
     
-fix_start("babble")
+def mix_up(a, b):
+        
+    t1=a[0:2]
+    t2=b[0:2]
     
+    
+    a=t2+a[2:]
+    b=t1+b[2:]
+    return a+" "+b
+
+    
+def main():
+  print ('donuts')
+  # Each line calls donuts, compares its result to the expected for that call.
+  test(donuts(4), 'Number of donuts: 4')
+  test(donuts(9), 'Number of donuts: 9')
+  test(donuts(10), 'Number of donuts: many')
+  test(donuts(99), 'Number of donuts: many')
+
+  print
+  print ('both_ends')
+  test(both_ends('spring'), 'spng')
+  test(both_ends('Hello'), 'Helo')
+  test(both_ends('a'), '')
+  test(both_ends('xyz'), 'xyyz')
+
+  
+  print
+  print ('fix_start')
+  test(fix_start('babble'), 'ba**le')
+  test(fix_start('aardvark'), 'a*rdv*rk')
+  test(fix_start('google'), 'goo*le')
+  test(fix_start('donut'), 'donut')
+
+  print
+  print ('mix_up')
+  test(mix_up('mix', 'pod'), 'pox mid')
+  test(mix_up('dog', 'dinner'), 'dig donner')
+  test(mix_up('gnash', 'sport'), 'spash gnort')
+  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+  main()
+    
+
+    
+ ''' Sample Output : 
+ donuts
+ OK  got: 'Number of donuts: 4' expected: 'Number of donuts: 4'
+ OK  got: 'Number of donuts: 9' expected: 'Number of donuts: 9'
+ OK  got: 'Number of donuts: many' expected: 'Number of donuts: many'
+ OK  got: 'Number of donuts: many' expected: 'Number of donuts: many'
+both_ends
+ OK  got: 'spng' expected: 'spng'
+ OK  got: 'Helo' expected: 'Helo'
+ OK  got: '' expected: ''
+ OK  got: 'xyyz' expected: 'xyyz'
+fix_start
+ OK  got: 'ba**le' expected: 'ba**le'
+ OK  got: 'a*rdv*rk' expected: 'a*rdv*rk'
+ OK  got: 'goo*le' expected: 'goo*le'
+ OK  got: 'donut' expected: 'donut'
+mix_up
+ OK  got: 'pox mid' expected: 'pox mid'
+ OK  got: 'dig donner' expected: 'dig donner'
+ OK  got: 'spash gnort' expected: 'spash gnort'
+ OK  got: 'fizzy perm' expected: 'fizzy perm'
+ '''
